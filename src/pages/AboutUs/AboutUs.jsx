76338cd17ec051086d4fcaea5645cf1ee89df0d5
Muhammad Junaid <junnedbutt@gmail.com>
2025-12-30 10:19:41 +0500

    fixing import issue for vercel

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CoreFeatureAbout from "./CoreFeatureAbout";
 import WelcomeSection from "../Home/WelcomeSection";
-import aboutHeroImage from '../../assets/hotelBg.webp' 
+import aboutHeroImage from '../../assets/hotelbg.webp' 
 import ChooseUs from "./ChooseUs";
 import HeroSection from "../../components/HeroSection";
 
